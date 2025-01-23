25705ed244588245c8355d53fd793e3fdb350e1f
smit4450 <smit4450@purdue.edu>
2025-01-23 10:59:38 -0500

    intitialized project

---
+++ apps/backend/src/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello from the backend!');
+});
+
+app.listen(3001, () => {
+    console.log('Backend running on http://localhost:3001');
+});
