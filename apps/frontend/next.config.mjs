68a1a0b18f61e4aa45897e1618ea046629572356
ivandrop <ivanshant@gmail.com>
2025-01-30 21:48:46 -0500

    enable static export

--- apps/frontend/next.config.mjs
+++ apps/frontend/next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: "export", // Enables static site export (required for GitHub Pages)
+    distDir: "out",   // Ensures build output is in 'out' (GitHub Pages requires this)
+};
 
 export default nextConfig;
