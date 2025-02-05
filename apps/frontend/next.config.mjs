3ab6e283ceba97d5413d7aa6be9179a364c9e056
ivandrop <ivanshant@gmail.com>
2025-02-05 01:06:46 -0500

    quick fix

--- apps/frontend/next.config.mjs
+++ apps/frontend/next.config.mjs
@@ -2,7 +2,8 @@
 const nextConfig = {
     // Github Pages Setup
     // output: "export", // Enables static site export (required for GitHub Pages)
-    // distDir: "out",   // Ensures build output is in 'out' (GitHub Pages requires this)
+    output: "standalone",
+    distDir: "out"   // Ensures build output is in 'out' (GitHub Pages requires this)
 };
 
 export default nextConfig;
